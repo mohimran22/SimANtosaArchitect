0d2a1cda82096903fe524b436c8211b0e42aeaa7
zoelhapner <achmadzulkiflinurrochim@gmail.com>
2026-02-02 16:19:24 +0700

    users

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -12,42 +12,50 @@ use App\Models\City;
 use App\Models\District;
 use App\Models\SubDistrict;
 use App\Models\PostalCode;
+use Spatie\Permission\Models\Role;
 
 class UserSeeder extends Seeder
 {
     public function run(): void
     {
-        User::create([
-            'id' => Str::uuid(),
+        $user = User::firstOrCreate(
+            ['email' => 'admin@gmail.com'],
+            [
+                'id' => Str::uuid(),
 
-            'fullname' => 'Super Admin',
-            'nickname' => 'Admin',
-            'gender' => 1,
+                'fullname' => 'Super Admin',
+                'nickname' => 'Admin',
+                'gender' => 1,
 
-            'email' => 'superadmin@gmail.com',
-            'email_verified_at' => now(),
+                'email_verified_at' => now(),
+                'password' => Hash::make('password'),
 
-            'password' => Hash::make('password'),
+                'birth_place' => 'Jakarta',
+                'identity_number' => '1234567890123456',
+                'birth_date' => '1990-01-01',
 
-            'birth_place' => 'Jakarta',
-            'identity_number' => '1234567890123456',
-            'birth_date' => '1990-01-01',
+                'religion_id' => \App\Models\Religion::first()?->id,
+                'province_id' => 5,
+                'city_id' => 91,
+                'district_id' => 1104,
+                'sub_district_id' => 15935,
+                'postal_code_id' => 15935,
 
-            // ambil FK pertama supaya aman
-            'religion_id' => Religion::first()->id ?? 1,
-            'province_id' => 5,
-            'city_id' => 91,
-            'district_id' => 1104,
-            'sub_district_id' => 15935,
-            'postal_code_id' => 15935,
+                'address' => 'Jl. Contoh Alamat No. 1',
+                'phone' => '08123456789',
 
-            'address' => 'Jl. Contoh Alamat No. 1',
-            'phone' => '08123456789',
+                'photo' => null,
+                'identity_photo' => null,
 
-            'photo' => null,
-            'identity_photo' => null,
+                'remember_token' => Str::random(10),
+            ]
+        );
 
-            'remember_token' => Str::random(10),
-        ]);
+        // 🔥 Assign role Super-Admin
+        $role = Role::where('name', 'Super-Admin')->first();
+
+        if ($role && !$user->hasRole($role->name)) {
+            $user->assignRole($role);
+        }
     }
 }
