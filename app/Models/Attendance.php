6f71d99105c02c40393ad9a95959623afd79f45c
zoelhapner <achmadzulkiflinurrochim@gmail.com>
2026-08-07 12:37:16 +0700

    hapus sofdelete

--- app/Models/Attendance.php
+++ app/Models/Attendance.php
@@ -5,11 +5,11 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\SoftDeletes;
+// use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Attendance extends Model
 {
-    use HasUuids, SoftDeletes;
+    use HasUuids;
 
     protected $keyType = 'string';
     public $incrementing = false;
